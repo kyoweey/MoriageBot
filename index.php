b3f099798bae002be60e35c67d60c6132005e599
kyoei oikawa <kyoei616june@gmail.com>
2018-03-06 01:23:09 +0900

    test

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/factory/MoriageControllerFactory.php';
 require_once __DIR__ . '/factory/ReplyFactory.php';
 require_once __DIR__ . '/view/Reply.php';
 require_once __DIR__ . '/controller/MoriageController.php';
+require_once __DIR__ . '/Dispatcher.php';
 
 $dispatcher = DispatcherFactory::create();
 $dispatcher->dispatch();
